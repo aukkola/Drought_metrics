56c3ee5491b8f43b78bc05bcbbdadc721da67739
Anna Ukkola <a.ukkola@unsw.edu.au>
2021-06-08 13:27:28 +1000

    indexing fix to function for last day

--- functions/drought_end.py
+++ functions/drought_end.py
@@ -4,12 +4,19 @@ def drought_end(vec, dry_day_ind, threshold):
     
     import numpy as np
     
-    for k in range(dry_day_ind+1, len(vec)): #need len(vec) here because stupid python ignores last number of range
+    #If last day, just return index
+    if dry_day_ind == len(vec)-1:
+        return dry_day_ind
+    
+    #Otherwise find date when exceeds threshold
+    else :
+    
+        for k in range(dry_day_ind+1, len(vec)): #need len(vec) here because stupid python ignores last number of range
         
-        #Check if time step exceeds threshold
-        exceeds = vec[k] > threshold[k]
+            #Check if time step exceeds threshold
+            exceeds = vec[k] > threshold[k]
         
-        if exceeds or k==(len(vec)-1): #need len(vec)-1 here 
-            return np.arange(dry_day_ind+1, k)
+            if exceeds or k==(len(vec)-1): #need len(vec)-1 here 
+                return np.arange(dry_day_ind+1, k)
             
             
